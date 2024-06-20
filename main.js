6ce8a664fc10b6819b5af1e6828ad8a11d147a06
Trilha <141366739+TrilhaX@users.noreply.github.com>
2024-06-20 09:16:27 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,4 @@
-function somar(n1, n2) {
-  // Convertendo n1 e n2 para números usando parseFloat
-  let numero1 = parseFloat(n1);
-  let numero2 = parseFloat(n2);
-  
+function somar(n1, n2) {  
   // Verificando se a conversão deu certo (não é NaN)
   if (!isNaN(numero1) && !isNaN(numero2)) {
     let soma = numero1 + numero2;
@@ -16,6 +12,9 @@ let calculo = prompt("Digite que tipo de conta queres fazer:");
 if (calculo === "Somar") { // Correção: Adicionar parênteses em torno da condição do if
   let n1 = prompt(`Digite o valor de n1:`);
   let n2 = prompt(`Digite o valor de n2:`);
+
+  n1 = parseInt(n1);
+  n2 = parseInt(n2);
 
   somar(n1, n2);
 } else {
