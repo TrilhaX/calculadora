e93630591548ff44dd076efe9a6f3d124098e602
Trilha <141366739+TrilhaX@users.noreply.github.com>
2024-06-20 09:30:39 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
-function somar(n1, n2) {
-  // Verificando se a conversão deu certo (não é NaN)
+let calculo = prompt("Digite que tipo de conta queres fazer:");
+if (calculo === "Somar") {
+  var n1 = prompt(`Digite o valor de n1:`);
+  var n2 = prompt(`Digite o valor de n2:`);
+
   n1 = parseInt(n1);
   n2 = parseInt(n2);
   
@@ -9,13 +12,6 @@ function somar(n1, n2) {
   } else {
     alert("Por favor, digite números válidos.");
   }
-}
-
-let calculo = prompt("Digite que tipo de conta queres fazer:");
-if (calculo === "Somar") {
-  var n1 = prompt(`Digite o valor de n1:`);
-  var n2 = prompt(`Digite o valor de n2:`);
-  somar(n1, n2);
 } else {
   alert("Operação inválida. Por favor, escolha 'Somar'.");
 }
