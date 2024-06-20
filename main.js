aea60472541e1071d651dcd78313c2d16d063c3d
Trilha <141366739+TrilhaX@users.noreply.github.com>
2024-06-20 09:19:08 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 function somar(n1, n2) {  
   // Verificando se a conversão deu certo (não é NaN)
+  var n1 = prompt(`Digite o valor de n1:`);
+  var n2 = prompt(`Digite o valor de n2:`);
+
+  n1 = parseInt(n1);
+  n2 = parseInt(n2);
+  
   if (!isNaN(numero1) && !isNaN(numero2)) {
     let soma = numero1 + numero2;
     alert(`A soma é: ${soma}`);
@@ -10,12 +16,6 @@ function somar(n1, n2) {
 
 let calculo = prompt("Digite que tipo de conta queres fazer:");
 if (calculo === "Somar") { // Correção: Adicionar parênteses em torno da condição do if
-  let n1 = prompt(`Digite o valor de n1:`);
-  let n2 = prompt(`Digite o valor de n2:`);
-
-  n1 = parseInt(n1);
-  n2 = parseInt(n2);
-
   somar(n1, n2);
 } else {
   alert("Operação inválida. Por favor, escolha 'Somar'.");
