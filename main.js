e653e7d3043a86de57777fb3e7f53f70dfcc0119
Trilha <141366739+TrilhaX@users.noreply.github.com>
2024-06-20 09:35:48 -0300

    Update main.js

--- main.js
+++ main.js
@@ -12,6 +12,19 @@ if (calculo === "Somar") {
   } else {
     alert("Por favor, digite números válidos.");
   }
+  if (calculo === "Subtração") {
+  var n1 = prompt(`Digite o valor de n1:`);
+  var n2 = prompt(`Digite o valor de n2:`);
+
+  n1 = parseInt(n1);
+  n2 = parseInt(n2);
+  
+  if (!isNaN(n1) && !isNaN(n2)) { // Corrigido para n1 e n2
+    let sub = n1 - n2; // Corrigido para n1 e n2
+    alert(`A Subtração é: ${sub}`);
+  } else {
+    alert("Por favor, digite números válidos.");
+  }
 } else {
   alert("Operação inválida. Por favor, escolha 'Somar'.");
 }
