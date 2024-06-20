bb5d8fdc98ff58096615405b3ed110f6ea1751e5
Trilha <141366739+TrilhaX@users.noreply.github.com>
2024-06-20 10:59:15 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 let calculo = prompt("Digite que tipo de conta queres fazer:");
 
-if (calculo === "Somar") {
+if (calculo === "Soma") {
   var n1 = prompt(`Digite o valor de n1:`);
   var n2 = prompt(`Digite o valor de n2:`);
 
@@ -24,6 +24,34 @@ if (calculo === "Somar") {
   
   if (!isNaN(n1) && !isNaN(n2)) {
     let sub = n1 - n2;
+    alert(`A Subtração é: ${sub.toFixed(2).replace('.', ',')}`); // Formatar saída com vírgula
+  } else {
+    alert("Por favor, digite números válidos.");
+  }
+  } else if (calculo === "Multiplicação") {
+  var n1 = prompt(`Digite o valor de n1:`);
+  var n2 = prompt(`Digite o valor de n2:`);
+
+  // Substituir vírgulas por pontos e converter para float
+  n1 = parseFloat(n1.replace(',', '.'));
+  n2 = parseFloat(n2.replace(',', '.'));
+  
+  if (!isNaN(n1) && !isNaN(n2)) {
+    let sub = n1 * n2;
+    alert(`A Subtração é: ${sub.toFixed(2).replace('.', ',')}`); // Formatar saída com vírgula
+  } else {
+    alert("Por favor, digite números válidos.");
+  }
+  } else if (calculo === "Divisão") {
+  var n1 = prompt(`Digite o valor de n1:`);
+  var n2 = prompt(`Digite o valor de n2:`);
+
+  // Substituir vírgulas por pontos e converter para float
+  n1 = parseFloat(n1.replace(',', '.'));
+  n2 = parseFloat(n2.replace(',', '.'));
+  
+  if (!isNaN(n1) && !isNaN(n2)) {
+    let sub = n1/n2;
     alert(`A Subtração é: ${sub.toFixed(2).replace('.', ',')}`); // Formatar saída com vírgula
   } else {
     alert("Por favor, digite números válidos.");
