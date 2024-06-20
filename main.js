daa1009cd7d26a0c01099e62a25dae7573df40e7
Trilha <141366739+TrilhaX@users.noreply.github.com>
2024-06-20 09:42:01 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 let calculo = prompt("Digite que tipo de conta queres fazer:");
+
 if (calculo === "Somar") {
   var n1 = prompt(`Digite o valor de n1:`);
   var n2 = prompt(`Digite o valor de n2:`);
@@ -6,25 +7,25 @@ if (calculo === "Somar") {
   n1 = parseInt(n1);
   n2 = parseInt(n2);
   
-  if (!isNaN(n1) && !isNaN(n2)) { // Corrigido para n1 e n2
-    let soma = n1 + n2; // Corrigido para n1 e n2
+  if (!isNaN(n1) && !isNaN(n2)) {
+    let soma = n1 + n2;
     alert(`A soma é: ${soma}`);
   } else {
     alert("Por favor, digite números válidos.");
   }
-  else if (calculo === "Subtração") {
+} else if (calculo === "Subtração") {
   var n1 = prompt(`Digite o valor de n1:`);
   var n2 = prompt(`Digite o valor de n2:`);
 
   n1 = parseInt(n1);
   n2 = parseInt(n2);
   
-  if (!isNaN(n1) && !isNaN(n2)) { // Corrigido para n1 e n2
-    let sub = n1 - n2; // Corrigido para n1 e n2
+  if (!isNaN(n1) && !isNaN(n2)) {
+    let sub = n1 - n2;
     alert(`A Subtração é: ${sub}`);
   } else {
     alert("Por favor, digite números válidos.");
   }
 } else {
-  alert("Operação inválida. Por favor, escolha 'Somar'.");
+  alert("Operação inválida. Por favor, escolha 'Somar' ou 'Subtração'.");
 }
