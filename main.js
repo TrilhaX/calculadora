89a2b97816e464cc9419bcc40023d2fceb07bcdb
Trilha <141366739+TrilhaX@users.noreply.github.com>
2024-06-20 12:08:32 -0300

    Update main.js

--- main.js
+++ main.js
@@ -56,6 +56,32 @@ if (calculo === "Soma") {
   } else {
     alert("Por favor, digite números válidos.");
   }
+} else if (calculo === "Potencia") {
+  var n1 = prompt(`Digite o valor da Base:`);
+  var n2 = prompt(`Digite o valor do Expoente:`);
+
+  // Substituir vírgulas por pontos e converter para float
+  n1 = parseFloat(n1.replace(',', '.'));
+  n2 = parseFloat(n2.replace(',', '.'));
+  
+  if (!isNaN(n1) && !isNaN(n2)) {
+    let sub = n1 ** n2;
+    alert(`A Potencia é: ${sub.toFixed(2).replace('.', ',')}`); // Formatar saída com vírgula
+  } else {
+    alert("Por favor, digite números válidos.");
+  }
+} else if (calculo === "Raiz Quadrada") {
+  var n1 = prompt(`Digite o valor da Base:`);
+
+  // Substituir vírgulas por pontos e converter para float
+  n1 = parseFloat(n1.replace(',', '.'));
+  
+  if (!isNaN(n1) && !isNaN(n2)) {
+    let RaizQuadrada = Math.sqrt(n1);
+    alert(`A Raiz Quadrada é: ${sub.toFixed(2).replace('.', ',')}`); // Formatar saída com vírgula
+  } else {
+    alert("Por favor, digite números válidos.");
+  }
 } else {
   alert("Operação inválida. Por favor, escolha 'Somar' ou 'Subtração'.");
 }
