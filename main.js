d78bdb63bd1629215fc86751d5d63bee1d2f330e
Trilha <141366739+TrilhaX@users.noreply.github.com>
2024-07-03 10:42:55 -0300

    Update main.js

--- main.js
+++ main.js
@@ -222,30 +222,40 @@ function performCalculation(operation) {
       }
       break;
 
-     case "20":
+      case "20":
         var Types = prompt(`Digite o tipo da matriz (2x2 ou 3x3): `);
-
+    
         if (Types === "2x2") {
             var a1 = parseNumberInput(prompt(`Digite o Primeiro Valor: `));
             var a2 = parseNumberInput(prompt(`Digite o Segundo Valor: `));
             var a3 = parseNumberInput(prompt(`Digite o Terceiro Valor: `));
             var a4 = parseNumberInput(prompt(`Digite o Quarto Valor: `));
-
+    
             var r1 = parseNumberInput(prompt(`Digite o Primeiro Resultado: `));
             var r2 = parseNumberInput(prompt(`Digite o Segundo Resultado: `));
-
+    
             var determinante = a1 * a4 - a2 * a3;
             var Dx = (r1 * a4 - r2 * a2);
             var Dy = (a1 * r2 - a3 * r1);
             var DxD = (r1 * a4 - r2 * a2)/determinante;
             var DyD = (a1 * r2 - a3 * r1)/determinante;
-
+    
             result = `O Determinante é: ${determinante.toFixed(2).replace('.', ',')}, Dx é ${Dx.toFixed(2).replace('.', ',')}/${determinante.toFixed(2).replace('.', ',')} e Dy é ${Dy.toFixed(2).replace('.', ',')}/${determinante.toFixed(2).replace('.', ',')}. Dx Simplificado: ${DxD.toFixed(2).replace('.', ',')}, Dy Simplificado: ${DyD.toFixed(2).replace('.', ',')}`;
         } else if (Types === "3x3") {
             var a1 = parseNumberInput(prompt(`Digite o Primeiro Valor: `));
             var a2 = parseNumberInput(prompt(`Digite o Segundo Valor: `));
+            var a3 = parseNumberInput(prompt(`Digite o Terceiro Valor: `));
+            var a4 = parseNumberInput(prompt(`Digite o Quarto Valor: `));
+            var a5 = parseNumberInput(prompt(`Digite o Quinto Valor: `));
+            var a6 = parseNumberInput(prompt(`Digite o Sexto Valor: `));
+            var a7 = parseNumberInput(prompt(`Digite o Sétimo Valor: `));
+            var a8 = parseNumberInput(prompt(`Digite o Oitavo Valor: `));
+            var a9 = parseNumberInput(prompt(`Digite o Nono Valor: `));
+    
+            var r1 = parseNumberInput(prompt(`Digite o Primeiro Resultado: `));
+            var r2 = parseNumberInput(prompt(`Digite o Segundo Resultado: `));
             var r3 = parseNumberInput(prompt(`Digite o Terceiro Resultado: `));
-
+    
             var determinante = (a1 * a5 * a9) + (a2 * a6 * a7) + (a3 * a4 * a8) - (a3 * a5 * a7) - (a1 * a6 * a8) - (a2 * a4 * a9);
             var Dx = (((r1 * a5 * a9) + (a2 * a6 * r3) + (a3 * r2 * a8)) - ((a3 * a5 * r3) + (r2 * a2 * a9) + (r1 * a6 * a8)));
             var Dy = (((a1 * r2 * a9) + (a2 * a6 * a7) + (a3 * a4 * r3)) - ((a3 * r2 * a7) + (a1 * a6 * r3) + (a4 * r1 * a9)));
@@ -253,12 +263,12 @@ function performCalculation(operation) {
             var DxD = (((r1 * a5 * a9) + (a2 * a6 * r3) + (a3 * r2 * a8)) - ((a3 * a5 * r3) + (r2 * a2 * a9) + (r1 * a6 * a8))) / determinante;
             var DyD = (((a1 * r2 * a9) + (a2 * a6 * a7) + (a3 * a4 * r3)) - ((a3 * r2 * a7) + (a1 * a6 * r3) + (a4 * r1 * a9))) / determinante;
             var DzD = ((a1 * a5 * r3) + (a2 * r2 * a7) + (r1 * a4 * a8) - ((r1 * a5 * a7) + (a1 * r2 * a8) + (a2 * a4 * r3))) / determinante;
-
+    
             result = `O Determinante é: ${determinante.toFixed(2).replace('.', ',')}, Dx é ${Dx.toFixed(2).replace('.', ',')}, Dy é ${Dy.toFixed(2).replace('.', ',')} e Dz é ${Dz.toFixed(2).replace('.', ',')}. X é: ${DxD.toFixed(2).replace('.', ',')}, Y é: ${DyD.toFixed(2).replace('.', ',')} e Z é : ${DzD.toFixed(2).replace('.', ',')}`;
       } else {
             alert("Tipo de matriz inválido. Por favor, digite '2x2' ou '3x3'.");
       }
-      break;
+    break;
 
     case "21": // Sequencia de Cores
       var pergunta = prompt("Quer Descobrir as Cores?(s/n): ")
